ceea18cbd03aa16ff244d34dc719a4f84674c385
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-12-31 08:36:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -385,7 +385,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://gyanilinks.com/api'
+    url = f'https://shareus.site/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
